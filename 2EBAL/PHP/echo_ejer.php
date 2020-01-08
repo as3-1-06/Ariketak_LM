308b3a6521f82ae59cc901fd6caacb92ecefa269
AS3-1-06 <AS3-1-06@uni.lan>
2020-01-08 08:27:28 +0100

    aaa

---
+++ 2EBAL/PHP/echo_ejer.php
@@ -0,0 +1,36 @@
+<?php
+
+//komentario
+/*komentario*/
+
+/*echo $_GET["Izena"];
+echo "<br>";
+echo $_GET["Abizena"];
+
+
+echo $_GET["Izena"];
+echo "<br>";
+echo $_GET["Pasahitza"];*/
+
+
+echo $_GET["gender"];
+
+
+/*if (isset($_GET["gender"]))
+    echo $_GET["gender"];
+
+echo "<br>";
+
+if (isset($_GET["gender1"]))
+    echo $_GET["gender1"];
+
+echo "<br>";
+
+if (isset($_GET["gender2"]))
+    echo $_GET["gender2"];*/
+
+
+echo $_GET ["Lenguaje"];
+
+
+echo $_GET ["message"];
